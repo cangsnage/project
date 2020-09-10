3ed7cf6862f9ced58de925156437d8ffa24b9c7e
helong <2804446445@qq.com>
2020-09-10 16:36:33 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+console.log(Vue)
+Vue.use(VueRouter)
+
+const routes = [
+
+]
+var router = new VueRouter({
+    routes
+})
+export default router
